756e49fb94776c3cd4262b63bfa72a3cfec41a54
Graham Hukill <ghukill@gmail.com>
2022-11-27 16:41:02 -0500

    cache gtp3 prompts

--- gpt3/gpt3.py
+++ gpt3/gpt3.py
@@ -3,8 +3,14 @@
 """
 
 import json
+import logging
+import os.path
+import uuid
 
 import openai
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 with open("gpt3/gpt3_config.json", "r") as f:
     config = json.loads(f.read())
@@ -30,6 +36,18 @@ def submit_prompt(prompt: str, temperature=1, max_tokens=MAX_TOKENS):
 
 def tldr(prompt, max_tokens=MAX_TOKENS) -> str:
 
-    response = submit_prompt(f'"{prompt}"\n\ntl;dr\n', max_tokens=max_tokens)
-    summary = response.to_dict()["choices"][0]["text"]
-    return summary.strip("\n")
+    prompt_uuid = str(uuid.uuid3(uuid.NAMESPACE_OID, prompt))
+    filename = f"tmp/{prompt_uuid}.gpt3"
+
+    if os.path.exists(filename):
+        logger.info(f"cache hit for prompt: {prompt_uuid}")
+        with open(filename, "r") as f:
+            return f.read()
+    else:
+        logger.info(f"cache miss for prompt: {prompt_uuid}")
+        response = submit_prompt(f'"{prompt}"\n\ntl;dr\n', max_tokens=max_tokens)
+        summary = response.to_dict()["choices"][0]["text"]
+        summary = summary.strip("\n")
+        with open(filename, "w") as f:
+            f.write(summary)
+        return summary
